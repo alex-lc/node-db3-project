90ebf3206ba1bbf84dc34a47c591013afce4bd73
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-02-19 16:44:39 -0600

    queries and some models complete

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,5 +1,43 @@
 const db = require('../data/dbConfig.js');
 
+module.exports = {
+    find,
+    findById,
+    add,
+    remove
+};
+
+// find all schemes
 function find() {
     return db('schemes');
+}
+
+// find scheme by id
+function findById(id) {
+    return db('schemes')
+        .where({ id })
+        .first();
+}
+
+// function findSteps(id) {
+//     return db('schemes')
+//         .where
+// }
+
+// create a new scheme
+function add(scheme) {
+    return db('schemes').insert(scheme, 'id');
+}
+
+// update a scheme
+// function update(id, changes) {
+//     return db('schemes')
+//         .where({ id })
+//         .update(changes);
+// }
+
+function remove(id) {
+    return db('schemes')
+        .where({ id })
+        .del();
 }
